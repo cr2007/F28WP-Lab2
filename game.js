8312d3b042f70f306af9088736319a6bd6cd5507
Chandrashekhar R <cr2007@hw.ac.uk>
2021-10-24 12:48:17 +0400

    Adds function to fit the bear to the board limits
    Uses DOM API to prevent the bear from going beyond the board
    and reshifts it back to the corner where it was

--- game.js
+++ game.js
@@ -29,3 +29,17 @@ function start() {
     document.getEventListener("keydown", moveBear, false);
 }
 
+this.fitBounds = function() {
+    let parent = this.htmlElement.parentElement;
+    let iw = this.htmlElement.offsetWidth;
+    let ih = this.htmlElement.offsetHeight;
+    let l = parent.offsetLeft;
+    let t = parent.offsetTop;
+    let w = parent.offsetWidth;
+    let h = parent.offsetHeight;
+
+    if(this.x < 0) this.x = 0;
+    if(this.x > w - iw) this.x = w-iw;
+    if(this.y < 0) this.y = 0;
+    if(this.y > h - ih) this.y = h - ih;
+};
